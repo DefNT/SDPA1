2e2e9e107cea763072783a9fef13d0a589d895e4
DefNotT <turlyhanzortbas@gmail.com>
2026-09-10 17:44:27 +0500

    add car product class

---
+++ src/Car.java
@@ -0,0 +1,19 @@
+public class Car {
+    private String engine;
+    private String transmission;
+    private String interior;
+    private int seats;
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+    public void setInterior(String interior) {
+       this.interior = interior;
+    }
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+}
